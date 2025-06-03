21f0f52f16a682d50130d614f0f09b68ad3cf7ef
Ali Ugur <aliugur997@gmail.com>
2025-06-03 17:24:19 +0300

    add tests

--- src/test_rule_engine/test_rule_50.rs
+++ src/test_rule_engine/test_rule_50.rs
@@ -14,3 +14,55 @@ fn general_test(code: &str, expected: Vec<String>) {
         );
     }
 }
+#[test]
+fn test_rule_50_method_raises_single_exc_docstring_no_raises_section() {
+    let code: &str = r#"
+class Class1:
+    """Docstring."""
+    def function_1(self):
+        """Docstring 1."""
+        raise Exc1
+"#;
+    let expected: Vec<String> = vec![format!("5:8 {}", raises_section_not_in_docstr_msg())];
+    general_test(code, expected);
+}
+
+#[test]
+fn test_rule_50_function_raises_single_exc_docstring_no_raises_section() {
+    let code: &str = r#"
+def function_1():
+    """Docstring 1."""
+    raise Exc1
+"#;
+    let expected: Vec<String> = vec![format!("3:4 {}", raises_section_not_in_docstr_msg())];
+    general_test(code, expected);
+}
+
+#[test]
+fn test_rule_50_private_function_raises_single_exc_docstring_no_raises_section() {
+    let code: &str = r#"
+def _function_1():
+    """Docstring 1."""
+    raise Exc1
+"#;
+    let expected: Vec<String> = vec![];
+    general_test(code, expected);
+}
+
+#[test]
+fn test_rule_50_multiple_function_raises_single_exc_docstring_no_raises_section() {
+    let code: &str = r#"
+def function_1():
+    """Docstring 1."""
+    raise Exc1
+
+def function_2():
+    """Docstring 2."""
+    raise Exc2
+"#;
+    let expected: Vec<String> = vec![
+        format!("3:4 {}", raises_section_not_in_docstr_msg()),
+        format!("7:4 {}", raises_section_not_in_docstr_msg()),
+    ];
+    general_test(code, expected);
+}
