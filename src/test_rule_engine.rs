814d563732bb0434278ce9231b7abd6edb8406ce
Ali Ugur <ali.ugur@canonical.com>
2025-11-07 16:39:01 +0300

    add more tests

--- src/test_rule_engine.rs
+++ src/test_rule_engine.rs
@@ -23,6 +23,7 @@ mod test_rule_62;
 mod test_rule_63;
 mod test_rule_64;
 mod test_rule_65;
+mod test_rule_6x;
 
 use crate::constants::{returns_section_in_docstr_msg, returns_section_not_in_docstr_msg};
 use crate::rule_engine::lint_file;
